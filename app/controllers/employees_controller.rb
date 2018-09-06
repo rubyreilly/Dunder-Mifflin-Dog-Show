d8a63ec43518c0c122c3879998174143b890824d
Garry Clerge <clergeg@gmail.com>
2018-09-06 14:38:49 -0400

    created solution branch and added landing page

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,30 @@
 class EmployeesController < ApplicationController
+
+
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+  end
+
+  def create
+  end
+
+  def delete
+    @employee = Employee.find(params[:id])
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :dogs
   resources :employees
+  get "/", to: "application#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
